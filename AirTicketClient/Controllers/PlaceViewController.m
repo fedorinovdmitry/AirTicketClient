d24307a4547643cac952d169b4ed9c8486cd58ce
Дмитрий Федоринов <skinklass@mail.ru>
2018-05-17 15:29:18 +0300

    добавил строку поиска городов и аэропортов

--- AirTicketClient/Controllers/PlaceViewController.m
+++ AirTicketClient/Controllers/PlaceViewController.m
@@ -11,12 +11,16 @@
 
 #define ReuseIdentifier @"CellIdentifier"
 
-@interface  PlaceViewController ()
+@interface  PlaceViewController () <UISearchResultsUpdating>
 
 @property (nonatomic) PlaceType placeType;
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) UISegmentedControl *segmentedControl;
-@property (nonatomic, strong) NSArray  *currentArray;
+@property (nonatomic, strong) NSArray *currentArray;
+@property (nonatomic, strong) NSArray *searchArray;
+@property (nonatomic, strong) UISearchController *searchController;
+
+
 @end
 
 @implementation PlaceViewController
@@ -36,11 +40,36 @@
     
     self. navigationController.navigationBar.tintColor = [UIColor blueColor];
     
+    [self configSearchController];
+    [self configTableView];
+    [self configSegmentControl];
+    
+    
+    
+    
+    self.title = (_placeType == PlaceTypeDeparture) ? @"From" :@"To";
+    
+}
+-(void)configSearchController{
+    _searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+    _searchController.dimsBackgroundDuringPresentation = NO;
+    _searchController.searchResultsUpdater = self;
+    _searchArray = [NSArray new];
+}
+
+-(void)configTableView{
     _tableView = [[UITableView alloc] initWithFrame: self.view.bounds style: UITableViewStylePlain];
     _tableView.delegate = self;
     _tableView.dataSource = self;
+    if(@available(iOS 11.0, *)){
+        self.navigationItem.searchController = _searchController;
+    }else{
+        _tableView.tableHeaderView = _searchController.searchBar;
+    }
     [self.view addSubview:_tableView];
-    
+}
+
+-(void)configSegmentControl{
     _segmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"Cities",  @"Airports"]];
     [_segmentedControl addTarget:self
                           action:@selector(changeSource)
@@ -50,11 +79,7 @@
     self.navigationItem.titleView = _segmentedControl;
     _segmentedControl.selectedSegmentIndex = 0;
     [self changeSource];
-    
-    self.title = (_placeType == PlaceTypeDeparture) ? @"From" :@"To";
-    
 }
-
 - (void) changeSource {
     switch (_segmentedControl.selectedSegmentIndex){
         case  0:
@@ -68,10 +93,24 @@
     }
     [self.tableView reloadData];
 }
+#pragma mark - UISearchResultsUpdating
 
+-(void)updateSearchResultsForSearchController:(UISearchController *)searchController{
+    if(searchController.searchBar.text){
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF.name CONTAINS[cd] %@", searchController.searchBar.text];
+        _searchArray = [_currentArray filteredArrayUsingPredicate:predicate];
+        [_tableView reloadData];
+    }
+}
+-(BOOL)isSearching{
+    return _searchController.isActive && [_searchArray count]>0;
+}
 #pragma mark - UITableViewDataSource
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger) section {
+    if([self isSearching]){
+        return [_searchArray count];
+    }
     return [_currentArray count];
 }
 
@@ -83,10 +122,11 @@
         cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleSubtitle reuseIdentifier:ReuseIdentifier];
         cell.accessoryType =  UITableViewCellAccessoryDisclosureIndicator;
     }
+    
     [cell.detailTextLabel setFont:[UIFont fontWithName:@"Superclarendon-Light" size:11.0]];
     [cell.textLabel setFont:[UIFont fontWithName:@"SnellRoundhand-Bold" size:20.0]];
     
-    id <PlaceProtocol> place = _currentArray[indexPath.row];
+    id <PlaceProtocol> place = [self isSearching] ? [_searchArray objectAtIndex:indexPath.row] : [_currentArray objectAtIndex:indexPath.row];
     cell.textLabel.text = place.name;
     cell.detailTextLabel.text = place.code;
     
@@ -99,9 +139,16 @@
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     DataSourceType dataType = ((int)_segmentedControl.selectedSegmentIndex) +  1;
+    if([self isSearching]){
+        [self.delegate selectPlace:_searchArray[indexPath.row] withType:_placeType                                                                                                                                                             andDataType:dataType];
+        _searchController.active = NO;
+    }else{
+        [self.delegate selectPlace:_currentArray[indexPath.row] withType:_placeType                                                                                                                                                             andDataType:dataType];
+    }
     
-    [self.delegate selectPlace:_currentArray[indexPath.row] withType:_placeType                                                                                                                                                             andDataType:dataType];
     [self.navigationController popViewControllerAnimated: YES];
 }
+
+
 
 @end
